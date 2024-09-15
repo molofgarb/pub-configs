ec7ac8c38fcaef92cea2e376788bd7f1e6ab8dad
molofgarb <50849715+molofgarb@users.noreply.github.com>
2024-09-14 22:49:51 -0400

    changes made
    Signed-off-by: molofgarb <50849715+molofgarb@users.noreply.github.com>

--- pc-dotfiles/Microsoft.PowerShell_profile.ps1
+++ pc-dotfiles/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,6 @@ Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
 function prompt {
     $gitStatus = if ($(git status 2> $null)) { "`e[91m:" + (git rev-parse --abbrev-ref HEAD) }
-    Write-Host "`e[92m$env:username@$(($env:computername).ToLower()) `e[93m$($pwd.Path) `e[36m$LastExitCode$gitStatus"
+    Write-Host "`e[92m$env:username@$(($env:computername).ToLower()) `e[93m$($pwd.Path)$gitStatus `e[36m$LastExitCode"
     return "`e[92m$ `e[37m"
 }
