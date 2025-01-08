6c7ef2862648947e9a62e203e243e4cf0fc4406c
molofgarb <50849715+molofgarb@users.noreply.github.com>
2025-01-08 04:45:55 +0000

    Update Microsoft.PowerShell_profile.ps1

--- dotfiles-pc/Microsoft.PowerShell_profile.ps1
+++ dotfiles-pc/Microsoft.PowerShell_profile.ps1
@@ -9,3 +9,9 @@ function prompt {
     Write-Host "`e[92m$env:username@$(($env:computername).ToLower()) `e[93m$($pwd.Path)$gitStatus `e[36m$LastExitCode"
     return "`e[92m$ `e[37m"
 }
+
+function profile-update {
+    (curl -w '%{http_code}\n' https://raw.githubusercontent.com/molofgarb/molofgarb-system-scripts/refs/heads/main/dotfiles-pc/Microsoft.PowerShell_profile.ps1 -o NUL) -contains '200'
+    curl https://raw.githubusercontent.com/molofgarb/molofgarb-system-scripts/refs/heads/main/dotfiles-pc/Microsoft.PowerShell_profile.ps1 -o ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+    & $profile
+}
